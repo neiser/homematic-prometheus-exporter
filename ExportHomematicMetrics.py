4b9d88a6a7cf4d65b90990d73485e3f508d03c74
Andreas Neiser <andreas.neiser@gmail.com>
2019-10-03 11:52:50 +0200

    Finish first version of export script

--- ExportHomematicMetrics.py
+++ ExportHomematicMetrics.py
@@ -1,6 +1,10 @@
+import os
+import time
 import xml.etree.ElementTree as ET
 
 import requests
+from prometheus_client import start_http_server, PLATFORM_COLLECTOR, PROCESS_COLLECTOR, GC_COLLECTOR
+from prometheus_client.core import GaugeMetricFamily, REGISTRY
 
 MAP_METRICS = {
     '1.ACTUAL_TEMPERATURE': {'name': 'current_temperature_celsius'},
@@ -9,20 +13,49 @@ MAP_METRICS = {
     '10.STATE': {'name': 'heating_valve_open', 'type':'boolean'}
 }
 
-r = requests.get(url = 'http://homematic/addons/xmlapi/statelist.cgi')
-r.raise_for_status()
+def parse_value(value, type):
+    if type == 'boolean':
+        if value == 'true':
+            return 1
+        elif value == 'false':
+            return 0
+        else:
+            raise ValueError("Cannot parse '{}' to boolean value".format(value))
+    else:
+        return value
 
-root = ET.fromstring(r.text)
 
-for device in root:
-    room_name = device.attrib.get('name')
-    print("Device '{}'".format(room_name))
-    for channel in device:
-        for data_point in channel:
-            if data_point.tag != 'datapoint':
-                continue
-            data_point_name = data_point.attrib.get('name')
-            data_point_type = data_point_name[data_point_name.rindex(':')+1:]
-            metric = MAP_METRICS.get(data_point_type)
-            if metric is not None:
-                print("Metric {} has value {}".format(metric['name'], data_point.get('value')))
+def collect_metrics_from_homematic():
+    r = requests.get(url = 'http://{}/addons/xmlapi/statelist.cgi'.format(os.environ['HOMEMATIC_HOST']))
+    r.raise_for_status()
+    root = ET.fromstring(r.text)
+    for device in root:
+        room_name = device.attrib.get('name')
+        for channel in device:
+            for data_point in channel:
+                if data_point.tag != 'datapoint':
+                    continue
+                data_point_name = data_point.attrib.get('name')
+                data_point_type = data_point_name[data_point_name.rindex(':')+1:]
+                metric = MAP_METRICS.get(data_point_type)
+                if metric is not None:
+                    value = parse_value(data_point.get('value'), metric.get('type'))
+                    print("Metric {} has value {}".format(metric['name'], value))
+                    g = GaugeMetricFamily(metric['name'], "", labels=['room'])
+                    g.add_metric([room_name], value, data_point.attrib.get('timestamp'))
+                    yield g
+
+
+class CustomCollector(object):
+    def collect(self):
+        return list(collect_metrics_from_homematic())
+
+
+if __name__ == "__main__":
+    for c in [PROCESS_COLLECTOR, PLATFORM_COLLECTOR, GC_COLLECTOR]:
+        REGISTRY.unregister(c)
+    REGISTRY.register(CustomCollector())
+    start_http_server(8000)
+    print("Running...")
+    while True:
+        time.sleep(100)
